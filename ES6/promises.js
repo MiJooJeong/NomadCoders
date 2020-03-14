56fcd29b38b3819ab892aefedd8a18448bd848ff
mijoojeong <uuuio1224@gmail.com>
2020-03-14 18:37:05 +0900

    8.3 Chaining Promises

--- ES6/promises.js
+++ ES6/promises.js
@@ -23,3 +23,23 @@ amISexy
 
 
 // 8.3 Chaining Promises
+const practice = new Promise((resolve, reject) => {
+    resolve(2);
+});
+
+const timesTwo = number => number * 2;
+
+practice
+    .then(timesTwo)
+    .then(timesTwo)
+    .then(timesTwo)
+    .then(timesTwo)
+    .then(timesTwo)
+    .then(() => {
+        throw Error("Something is wrong")
+    })
+    .then(lastNumber => console.log(lastNumber))
+    .catch(error => console.log(error));
+
+
+// 8.4 Promise.all
